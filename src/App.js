f37ff87c57b5b7a91ed9cf6699789b5f6cfd7971
JohnMigwi <johnmigwi101@gmail.com>
2023-07-20 22:15:18 +0300

    removed headers

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './App.css';
-import Header from './Components/Header/Header'; 
+
 import Body from './Components/Body/Body';
-import Footer from './Components/Footer/Footer';
+
 
 function App() {
   return (
     <section className='phone'>
-      <Header />
+      
       <Body/>
-      <Footer/>
+      
     </section>
    
   );

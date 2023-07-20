9501980ca1508bdea4b7bccf6416849f7bd91587
JohnMigwi <johnmigwi101@gmail.com>
2023-07-20 16:45:56 +0300

    LowerBody sec 1

---
+++ src/Components/Body/Body.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import UpperBody from './UpperBody';
+import MiddleBody from './MiddleBody'
+import LowerBody from './LowerBody';
+
+const Body = () => {
+  return (
+    <>
+    <UpperBody/>
+    <MiddleBody/>
+    <LowerBody/>
+    </>
+  )
+}
+
+export default Body

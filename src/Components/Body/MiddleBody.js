f4ba552788a310a6677eb99cc9318c1c3b279034
JohnMigwi <johnmigwi101@gmail.com>
2023-07-20 18:46:47 +0300

    graph completed

--- src/Components/Body/MiddleBody.js
+++ src/Components/Body/MiddleBody.js
@@ -1,73 +1,41 @@
-import React, { PureComponent } from 'react';
+import React, { useState } from 'react';
 import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import data from '../../Resource/Data'; 
 
-const data = [
-  {
-    name: 'Page A',
-    uv: 1.8,
-    pv: 1,
-    amt: 1,
-  },
-  {
-    name: 'Page B',
-    uv: 2.4,
-    pv: 1,
-    amt: 1,
-  },
-  {
-    name: 'Page C',
-    uv: 2.3,
-    pv: 1,
-    amt: 1,
-  },
-  {
-    name: 'Page D',
-    uv: 2.5,
-    pv: 1,
-    amt: 1,
-  },
-  {
-    name: 'Page E',
-    uv: 2.2,
-    pv: 1,
-    amt: 1,
-  },
+const MiddleBody = () => {
+  const [period, setPeriod] = useState('week'); 
+  const handlePeriodChange = (newPeriod) => {
+    setPeriod(newPeriod);
+  };
 
-  {
-    name: 'Page G',
-    uv: 2.3,
-    pv: 1,
-    amt: 1,
-  },
-];
+  // Get the data for the selected period
+  const chartData = data[period];
 
-
-
-export default class Example extends PureComponent {
-  render() {
-    return (
-    <>
-    <div className='period'>
-      
-    </div>
-    <div className="middle-body">
-                <ResponsiveContainer width="100%" height="100%">
-                    <AreaChart
-                        width={200}
-                        height={60}
-                        data={data}
-                        margin={{
-                            top: 5,
-                            right: 0,
-                            left: 0,
-                            bottom: 5,
-                        }}
-                    >
-                        <Area type="monotone" dataKey="uv" stroke="#f7bc0a" fill="#fad157" />
-                    </AreaChart>
-                </ResponsiveContainer>
-    </div>
+  return (
+    <><div className="period-buttons">
+          <button onClick={() => handlePeriodChange('day')}>Day</button>
+          <button onClick={() => handlePeriodChange('week')}>Week</button>
+          <button onClick={() => handlePeriodChange('month')}>Month</button>
+          <button onClick={() => handlePeriodChange('year')}>Year</button>
+      </div><div className="middle-body">
+              <ResponsiveContainer width="100%" height="100%">
+                  <AreaChart
+                      width={200}
+                      height={60}
+                      data={chartData} 
+                      margin={{
+                          top: 5,
+                          right: 0,
+                          left: 0,
+                          bottom: 0,
+                      }}
+                  >
+                      <Area type="monotone" dataKey="uv" stroke="#f7bc0a" fill="#fad157" />
+                  </AreaChart>
+              </ResponsiveContainer>
+          </div>
     </>
-    );
-  }
-}
+  );
+};
+
+export default MiddleBody;

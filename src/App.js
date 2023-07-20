8b47195c3fb6edb836b87552098669ef38764d02
Kiprotich Shadrack <shadrackr057@gmail.com>
2023-07-20 19:05:53 +0300

    completed the footer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './Components/Header'; 
 import MiddleBody from './Components/Body/UpperBody'
-import Footer from './Components/Footer/Footer';
+import Footer from './Components/Footer';
 
 function App() {
   return (

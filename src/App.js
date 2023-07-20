debee8939b61fb4280efb4f0c466c2aa08d8a9aa
Kiprotich Shadrack <shadrackr057@gmail.com>
2023-07-20 22:20:29 +0300

    removed upper body from app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './Components/Header'; 
-import MiddleBody from './Components/Body/UpperBody'
+import Body from './Components/Body/UpperBody'
 import Footer from './Components/Footer';
 
 function App() {
   return (
     <section className='phone'>
       <Header />
-      <MiddleBody/>
+      <Body/>
       <Footer/>
     </section>
    
---
+++ src/Components/Body/Body.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import UpperBody from './UpperBody'
+
+const Body = () => {
+  return (
+    <>
+        <UpperBody />
+    </>
+  )
+}
+
+export default Body
